650df240103aad4b4adaf2da18016f6c1fe8cf97
AriaMoradi <aria.moradi007@gmail.com>
2024-03-14 22:33:55 +0330

    initial

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,46 @@
+import Dexie , { Table } from 'dexie';
 import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.ts'
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
+const appEl = document.querySelector('#app')!;
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+interface Building {
+  id: number;
+  name: string;
+  constructor: string;
+}
+
+class DB extends Dexie {
+  buildings!: Table<Building>;
+
+  create = () => {
+    if (this.isOpen()) return;
+
+    this.version(1).stores({
+        buildings: 'id, name, constructor' 
+    });
+
+    if (!this.isOpen()) this.open();
+  };
+
+  constructor() {
+    super('buildings_db');
+    this.create();
+  }
+}
+
+var db = new DB();
+
+
+db.buildings.bulkAdd([
+  {
+    id: 1,
+    name: 'Building #1',
+    constructor: "Constructor company"
+  }
+])
+.then(() => db.buildings.toArray())
+.then((items) => {
+  appEl.innerHTML = `${appEl}`
+  console.log('shit')
+})
+
